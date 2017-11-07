553b17695839a5f5b4635df93e1e637ad2f76734
Caio Santos <caio-cesar-ms@hotmail.com>
2017-11-07 18:40:39 -0300

    first commit

---
+++ app/src/main/java/com/example/caioblade/battery/PowerConnectionReceiver.java
@@ -0,0 +1,24 @@
+package com.example.caioblade.battery;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.BatteryManager;
+
+/**
+ * Created by CaioBlade on 11/7/2017.
+ */
+
+public class PowerConnectionReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+            int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+            boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                    status == BatteryManager.BATTERY_STATUS_FULL;
+
+            int chargePlug = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+            boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
+            boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+    }
+}
+
